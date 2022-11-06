bdd5182e458e9d4fd6e0b7a0c03044db8d1d8d54
ChrisKim <1515748548@qq.com>
2022-11-06 14:24:13 +0800

    [练习] 集训队训练6 更正

--- WHUT/2022/Week06/E.cpp
+++ WHUT/2022/Week06/E.cpp
@@ -2,12 +2,15 @@
 
 using namespace std;
 
-const int MAXN = 1e5;
-int fa[MAXN + 10];
+const int MAXN = 1e5 + 10;
+int fa[MAXN], sz[MAXN];
 inline void init()
 {
-    for (int i = 1; i <= MAXN; i++)
+    for (int i = 0; i < MAXN; i++)
+    {
         fa[i] = i;
+        sz[i] = 1;
+    }
 }
 int find(int x)
 {
@@ -15,6 +18,9 @@ int find(int x)
 }
 inline void merge(int i, int j)
 {
+    if (find(i) == find(j))
+        return;
+    sz[find(j)] += sz[find(i)];
     fa[find(i)] = find(j);
 }
 
@@ -22,23 +28,30 @@ int main()
 {
     int a, b;
     init();
+    int cnt = 1, size = 1;
     bool flag = true;
     while (cin >> a >> b, a != -1 || b != -1)
     {
         if (!a && !b)
         {
-            if (flag)
+            // cout << cnt << ' ' << size << endl;
+            if (flag && size == cnt)
                 cout << "Yes" << endl;
             else
                 cout << "No" << endl;
             flag = true;
+            cnt = size = 1;
             init();
             continue;
         }
         if (flag && find(a) == find(b))
             flag = false;
         if (flag)
+        {
             merge(a, b);
+            size = sz[find(a)];
+            cnt++;
+        }
     }
     return 0;
 }
