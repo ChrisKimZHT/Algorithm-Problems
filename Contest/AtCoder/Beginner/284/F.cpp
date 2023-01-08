acddad6c62d6027126b19cb04ccacbf92263c4f1
ChrisKim <1515748548@qq.com>
2023-01-08 23:22:58 +0800

    [补题] AtCoder Beginner Contest 284

--- Contest/AtCoder/Beginner/284/F.cpp
+++ Contest/AtCoder/Beginner/284/F.cpp
@@ -4,52 +4,55 @@
 using namespace std;
 
 const int MAXN = 2e6 + 10;
-const unsigned long long MOD1 = 19260817, MOD2 = 19660813;
 int N;
-char T[MAXN], revT[MAXN];
+char T[MAXN], T_rev[MAXN];
 
-unsigned long long p[MAXN], h[MAXN], revh[MAXN];
-void init1()
+// hash-1: 19260817
+const unsigned long long MOD_1 = 19260817;
+unsigned long long p_1[MAXN], h_1[MAXN], h_rev_1[MAXN];
+void init_1()
 {
-    p[0] = 1;
+    p_1[0] = 1;
     for (int i = 1; i <= 2 * N; i++)
     {
-        p[i] = (p[i - 1] * 131) % MOD1;
-        h[i] = ((h[i - 1] * 131) % MOD1 + T[i]) % MOD1;
-        revh[i] = ((revh[i - 1] * 131) % MOD1 + revT[i]) % MOD1;
+        p_1[i] = (p_1[i - 1] * 131) % MOD_1;
+        h_1[i] = ((h_1[i - 1] * 131) % MOD_1 + T[i]) % MOD_1;
+        h_rev_1[i] = ((h_rev_1[i - 1] * 131) % MOD_1 + T_rev[i]) % MOD_1;
     }
 }
 
-unsigned long long get1(int l, int r)
+unsigned long long get_1(int l, int r)
 {
-    return (h[r] % MOD1 + MOD1 - (h[l - 1] * p[r - l + 1]) % MOD1) % MOD1;
+    return (h_1[r] % MOD_1 + MOD_1 - (h_1[l - 1] * p_1[r - l + 1]) % MOD_1) % MOD_1;
 }
 
-unsigned long long getrev1(int l, int r)
+unsigned long long get_rev_1(int l, int r)
 {
-    return (revh[r] % MOD1 + MOD1 - (revh[l - 1] * p[r - l + 1]) % MOD1) % MOD1;
+    return (h_rev_1[r] % MOD_1 + MOD_1 - (h_rev_1[l - 1] * p_1[r - l + 1]) % MOD_1) % MOD_1;
 }
 
-unsigned long long pp[MAXN], hh[MAXN], revhh[MAXN];
-void init2()
+// hash-2: 19660813
+const unsigned long long MOD_2 = 19660813;
+unsigned long long p_2[MAXN], h_2[MAXN], h_rev_2[MAXN];
+void init_2()
 {
-    pp[0] = 1;
+    p_2[0] = 1;
     for (int i = 1; i <= 2 * N; i++)
     {
-        pp[i] = (pp[i - 1] * 131) % MOD2;
-        hh[i] = ((hh[i - 1] * 131) % MOD2 + T[i]) % MOD2;
-        revhh[i] = ((revhh[i - 1] * 131) % MOD2 + revT[i]) % MOD2;
+        p_2[i] = (p_2[i - 1] * 131) % MOD_2;
+        h_2[i] = ((h_2[i - 1] * 131) % MOD_2 + T[i]) % MOD_2;
+        h_rev_2[i] = ((h_rev_2[i - 1] * 131) % MOD_2 + T_rev[i]) % MOD_2;
     }
 }
 
-unsigned long long get2(int l, int r)
+unsigned long long get_2(int l, int r)
 {
-    return (hh[r] % MOD2 + MOD2 - (hh[l - 1] * pp[r - l + 1]) % MOD2) % MOD2;
+    return (h_2[r] % MOD_2 + MOD_2 - (h_2[l - 1] * p_2[r - l + 1]) % MOD_2) % MOD_2;
 }
 
-unsigned long long getrev2(int l, int r)
+unsigned long long get_rev_2(int l, int r)
 {
-    return (revhh[r] % MOD2 + MOD2 - (revhh[l - 1] * pp[r - l + 1]) % MOD2) % MOD2;
+    return (h_rev_2[r] % MOD_2 + MOD_2 - (h_rev_2[l - 1] * p_2[r - l + 1]) % MOD_2) % MOD_2;
 }
 
 int main()
@@ -59,20 +62,20 @@ int main()
     cout.tie(0);
     cin >> N >> T + 1;
     for (int i = 2 * N; i >= 1; i--)
-        revT[2 * N - i + 1] = T[i];
-    init1();
-    init2();
+        T_rev[2 * N - i + 1] = T[i];
+    init_1();
+    init_2();
     for (int i = 0; i <= N; i++)
     {
         int l = N - i + 1, r = 2 * N - i;
-        unsigned long long a1 = getrev1(l, r);
-        unsigned long long b1 = ((1 <= i) ? get1(1, i) : 0);
-        unsigned long long c1 = ((i + N + 1 <= 2 * N) ? get1(i + N + 1, 2 * N) : 0);
-        unsigned long long a2 = getrev2(l, r);
-        unsigned long long b2 = ((1 <= i) ? get2(1, i) : 0);
-        unsigned long long c2 = ((i + N + 1 <= 2 * N) ? get2(i + N + 1, 2 * N) : 0);
-        if (a1 == ((b1 * p[N - i]) % MOD1 + c1) % MOD1 &&
-            a2 == ((b2 * pp[N - i]) % MOD2 + c2) % MOD2)
+        unsigned long long a_1 = get_rev_1(l, r),
+                           a_2 = get_rev_2(l, r),
+                           b_1 = get_1(1, i),
+                           b_2 = get_2(1, i),
+                           c_1 = get_1(i + N + 1, 2 * N),
+                           c_2 = get_2(i + N + 1, 2 * N);
+        if (a_1 == ((b_1 * p_1[N - i]) % MOD_1 + c_1) % MOD_1 &&
+            a_2 == ((b_2 * p_2[N - i]) % MOD_2 + c_2) % MOD_2)
         {
             for (int j = i + N; j >= i + 1; j--)
                 cout << T[j];
