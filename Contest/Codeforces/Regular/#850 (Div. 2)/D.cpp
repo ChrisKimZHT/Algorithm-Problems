4028900ff1cfebd9f7271433a206d83889cf869d
ChrisKim <1515748548@qq.com>
2023-02-06 23:59:23 +0800

    [比赛] Codeforces Round #850 (Div. 2, based on VK Cup 2022 - Final Round)

--- Contest/Codeforces/Regular/#850 (Div. 2)/D.cpp
+++ Contest/Codeforces/Regular/#850 (Div. 2)/D.cpp
@@ -7,7 +7,7 @@ void solve()
 {
     int m;
     cin >> m;
-    map<pair<char, char>, vector<int>> mp;
+    vector<int> v[6];
     vector<string> ans;
     for (int i = 1; i <= m; i++)
     {
@@ -25,122 +25,122 @@ void solve()
         }
         if (cnt_w == 3)
         {
-            mp[{'w', 'i'}].push_back(i);
-            mp[{'w', 'n'}].push_back(i);
+            v[0].push_back(i);
+            v[1].push_back(i);
         }
         else if (cnt_i == 3)
         {
-            mp[{'i', 'w'}].push_back(i);
-            mp[{'i', 'n'}].push_back(i);
+            v[2].push_back(i);
+            v[3].push_back(i);
         }
         else if (cnt_n == 3)
         {
-            mp[{'n', 'w'}].push_back(i);
-            mp[{'n', 'i'}].push_back(i);
+            v[4].push_back(i);
+            v[5].push_back(i);
         }
         else if (cnt_w == 2 && cnt_i == 0)
         {
-            mp[{'w', 'i'}].push_back(i);
+            v[0].push_back(i);
         }
         else if (cnt_w == 2 && cnt_n == 0)
         {
-            mp[{'w', 'n'}].push_back(i);
+            v[1].push_back(i);
         }
         else if (cnt_i == 2 && cnt_w == 0)
         {
-            mp[{'i', 'w'}].push_back(i);
+            v[2].push_back(i);
         }
         else if (cnt_i == 2 && cnt_n == 0)
         {
-            mp[{'i', 'n'}].push_back(i);
+            v[3].push_back(i);
         }
         else if (cnt_n == 2 && cnt_w == 0)
         {
-            mp[{'n', 'w'}].push_back(i);
+            v[4].push_back(i);
         }
         else if (cnt_n == 2 && cnt_i == 0)
         {
-            mp[{'n', 'i'}].push_back(i);
+            v[5].push_back(i);
         }
     }
-    for (auto now : mp[{'w', 'i'}])
+    for (auto now : v[0])
     {
-        if (mp[{'i', 'w'}].size())
+        if (v[2].size())
         {
-            int swp = mp[{'i', 'w'}].back();
-            mp[{'w', 'i'}].pop_back();
-            mp[{'i', 'w'}].pop_back();
+            int swp = v[2].back();
+            v[0].pop_back();
+            v[2].pop_back();
             ans.push_back(to_string(now) + " w " + to_string(swp) + " i");
         }
         else
         {
-            int swp = mp[{'n', 'w'}].back();
-            mp[{'w', 'i'}].pop_back();
-            mp[{'n', 'w'}].pop_back();
+            int swp = v[4].back();
+            v[0].pop_back();
+            v[4].pop_back();
             ans.push_back(to_string(now) + " w " + to_string(swp) + " n");
-            mp[{'n', 'i'}].push_back(now);
+            v[5].push_back(now);
         }
     }
-    for (auto now : mp[{'w', 'n'}])
+    for (auto now : v[1])
     {
-        if (mp[{'n', 'w'}].size())
+        if (v[4].size())
         {
-            int swp = mp[{'n', 'w'}].back();
-            mp[{'w', 'n'}].pop_back();
-            mp[{'n', 'w'}].pop_back();
+            int swp = v[4].back();
+            v[1].pop_back();
+            v[4].pop_back();
             ans.push_back(to_string(now) + " w " + to_string(swp) + " n");
         }
         else
         {
-            int swp = mp[{'i', 'w'}].back();
-            mp[{'w', 'n'}].pop_back();
-            mp[{'i', 'w'}].pop_back();
+            int swp = v[2].back();
+            v[1].pop_back();
+            v[2].pop_back();
             ans.push_back(to_string(now) + " w " + to_string(swp) + " i");
-            mp[{'i', 'n'}].push_back(now);
+            v[3].push_back(now);
         }
     }
-    for (auto now : mp[{'i', 'w'}])
+    for (auto now : v[2])
     {
-        if (mp[{'w', 'i'}].size())
+        if (v[0].size())
         {
-            int swp = mp[{'w', 'i'}].back();
-            mp[{'i', 'w'}].pop_back();
-            mp[{'w', 'i'}].pop_back();
+            int swp = v[0].back();
+            v[2].pop_back();
+            v[0].pop_back();
             ans.push_back(to_string(now) + " i " + to_string(swp) + " w");
         }
     }
-    for (auto now : mp[{'i', 'n'}])
+    for (auto now : v[3])
     {
-        if (mp[{'n', 'i'}].size())
+        if (v[5].size())
         {
-            int swp = mp[{'n', 'i'}].back();
-            mp[{'i', 'n'}].pop_back();
-            mp[{'n', 'i'}].pop_back();
+            int swp = v[5].back();
+            v[3].pop_back();
+            v[5].pop_back();
             ans.push_back(to_string(now) + " i " + to_string(swp) + " n");
         }
     }
-    for (auto now : mp[{'n', 'w'}])
+    for (auto now : v[4])
     {
-        if (mp[{'w', 'n'}].size())
+        if (v[1].size())
         {
-            int swp = mp[{'w', 'n'}].back();
-            mp[{'n', 'w'}].pop_back();
-            mp[{'w', 'n'}].pop_back();
+            int swp = v[1].back();
+            v[4].pop_back();
+            v[1].pop_back();
             ans.push_back(to_string(now) + " n " + to_string(swp) + " w");
         }
     }
-    for (auto now : mp[{'n', 'i'}])
+    for (auto now : v[5])
     {
-        if (mp[{'i', 'n'}].size())
+        if (v[3].size())
         {
-            int swp = mp[{'i', 'n'}].back();
-            mp[{'n', 'i'}].pop_back();
-            mp[{'i', 'n'}].pop_back();
+            int swp = v[3].back();
+            v[5].pop_back();
+            v[3].pop_back();
             ans.push_back(to_string(now) + " n " + to_string(swp) + " i");
         }
     }
     cout << ans.size() << endl;
-    for(auto ele:ans)
+    for (auto ele : ans)
         cout << ele << endl;
 }
 
