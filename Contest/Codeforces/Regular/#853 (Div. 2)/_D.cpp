1aea28410c9d03a8f0507c4cd5c93d9fdcb19305
ChrisKim <1515748548@qq.com>
2023-02-26 01:23:34 +0800

    [补题] Codeforces Round #853 (Div. 2)

---
+++ Contest/Codeforces/Regular/#853 (Div. 2)/_D.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string a, b;
+    cin >> a >> b;
+    if (a == b)
+    {
+        cout << 0 << endl;
+        return;
+    }
+    if (a.find('1') == a.npos ||
+        b.find('1') == b.npos)
+    {
+        cout << -1 << endl;
+        return;
+    }
+    string ra = a;
+    reverse(ra.begin(), ra.end());
+    vector<int> ans;
+    if (a[0] == '0')
+    {
+        int t = n - 1;
+        while (a[t] == '0')
+            t--;
+        ans.push_back(t);
+        a[0] = '1';
+    }
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] != b[i])
+        {
+            ans.push_back(-i);
+            string t = a;
+            for (int j = 0; j < n; j++)
+            {
+                if (i + j >= n)
+                    break;
+                if (t[j] == a[i + j])
+                    a[i + j] = '0';
+                else
+                    a[i + j] = '1';
+            }
+        }
+    }
+    if (b[0] == '0')
+    {
+        int t = n - 1;
+        while (a[t] == '0')
+            t--;
+        ans.push_back(t);
+        a[0] = '0';
+    }
+    if (ans.size() <= n)
+    {
+        cout << ans.size() << endl;
+        for (auto ele : ans)
+            cout << ele << ' ';
+        cout << endl;
+        return;
+    }
+    a = ra;
+    reverse(b.begin(), b.end());
+    ans.clear();
+    if (a[0] == '0')
+    {
+        int t = n - 1;
+        while (a[t] == '0')
+            t--;
+        ans.push_back(-t);
+        a[0] = '1';
+    }
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] != b[i])
+        {
+            ans.push_back(i);
+            string t = a;
+            for (int j = 0; j < n; j++)
+            {
+                if (i + j >= n)
+                    break;
+                if (t[j] == a[i + j])
+                    a[i + j] = '0';
+                else
+                    a[i + j] = '1';
+            }
+        }
+    }
+    if (b[0] == '0')
+    {
+        int t = n - 1;
+        while (a[t] == '0')
+            t--;
+        ans.push_back(-t);
+        a[0] = '0';
+    }
+    cout << ans.size() << endl;
+    for (auto ele : ans)
+        cout << ele << ' ';
+    cout << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
