65925097875fa40e614f9a5ffaa9b6d4048fd0e6
ChrisKim <1515748548@qq.com>
2023-02-26 13:15:18 +0800

    [补题] Codeforces Round #853 (Div. 2)

--- Contest/Codeforces/Regular/#853 (Div. 2)/_D.cpp
+++ Contest/Codeforces/Regular/#853 (Div. 2)/_D.cpp
@@ -3,26 +3,10 @@
 
 using namespace std;
 
-void solve()
+vector<int> getans(string a, string b)
 {
-    int n;
-    cin >> n;
-    string a, b;
-    cin >> a >> b;
-    if (a == b)
-    {
-        cout << 0 << endl;
-        return;
-    }
-    if (a.find('1') == a.npos ||
-        b.find('1') == b.npos)
-    {
-        cout << -1 << endl;
-        return;
-    }
-    string ra = a;
-    reverse(ra.begin(), ra.end());
     vector<int> ans;
+    int n = a.size();
     if (a[0] == '0')
     {
         int t = n - 1;
@@ -56,6 +40,27 @@ void solve()
         ans.push_back(t);
         a[0] = '0';
     }
+    return ans;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string a, b;
+    cin >> a >> b;
+    if (a == b)
+    {
+        cout << 0 << endl;
+        return;
+    }
+    if (a.find('1') == a.npos ||
+        b.find('1') == b.npos)
+    {
+        cout << -1 << endl;
+        return;
+    }
+    vector<int> ans = getans(a, b);
     if (ans.size() <= n)
     {
         cout << ans.size() << endl;
@@ -64,46 +69,14 @@ void solve()
         cout << endl;
         return;
     }
-    a = ra;
+    reverse(a.begin(), a.end());
     reverse(b.begin(), b.end());
-    ans.clear();
-    if (a[0] == '0')
-    {
-        int t = n - 1;
-        while (a[t] == '0')
-            t--;
-        ans.push_back(-t);
-        a[0] = '1';
-    }
-    for (int i = 1; i < n; i++)
-    {
-        if (a[i] != b[i])
-        {
-            ans.push_back(i);
-            string t = a;
-            for (int j = 0; j < n; j++)
-            {
-                if (i + j >= n)
-                    break;
-                if (t[j] == a[i + j])
-                    a[i + j] = '0';
-                else
-                    a[i + j] = '1';
-            }
-        }
-    }
-    if (b[0] == '0')
-    {
-        int t = n - 1;
-        while (a[t] == '0')
-            t--;
-        ans.push_back(-t);
-        a[0] = '0';
-    }
+    ans = getans(a, b);
     cout << ans.size() << endl;
     for (auto ele : ans)
-        cout << ele << ' ';
+        cout << -ele << ' ';
     cout << endl;
+    return;
 }
 
 int main()
